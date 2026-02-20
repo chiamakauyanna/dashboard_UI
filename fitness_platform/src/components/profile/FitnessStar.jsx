46380a80fb1b7c97dfa730a0dbd32f0766691b98
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2026-02-20 13:29:09 +0100

    Added fitness star

--- fitness_platform/src/components/profile/FitnessStar.jsx
+++ fitness_platform/src/components/profile/FitnessStar.jsx
@@ -1,7 +1,19 @@
+import award from "../../assets/trophy.png";
+
 const FitnessStar = () => {
   return (
-    <div className='rounded-[17px] bg-white shadow border p-4 mt-4 h-40.5'>FitnessStar</div>
-  )
-}
+    <div className="mt-4 flex h-40.5 items-center justify-between rounded-[17px] bg-[#F5BA4A]/30 px-6 shadow">
+      <div>
+        <p className="mb-1 text-[20px] font-bold">Fitness Star</p>
+        <p className="text-sm font-medium text-[#9D9D9D]">
+          Got an award for your calorie-burning journey.
+        </p>
+      </div>
+      <div>
+        <img src={award} alt="picture of a trophy" />
+      </div>
+    </div>
+  );
+};
 
-export default FitnessStar
+export default FitnessStar;
--- fitness_platform/src/components/profile/MusicProvider.jsx
+++ fitness_platform/src/components/profile/MusicProvider.jsx
@@ -1,7 +1,23 @@
 const MusicProvider = () => {
   return (
-    <div className='rounded-[17px] bg-white shadow border p-4 mt-4 h-65.5'>MusicProvider</div>
-  )
-}
+    <div className="mt-4 h-65.5 rounded-[17px] bg-white p-4 shadow">
+      <p className="text-[20px] font-bold">Music Provider</p>
+      <p className="text-sm font-medium text-[#9D9D9D]">
+        Listen to your favorite music while exercising. The best music streaming
+        services include access to millions of songs.
+      </p>
 
-export default MusicProvider
+      <div className="mb-8 space-y-2 px-2">
+        <div className="border-secondary border-b py-5">
+          <p className="font-semibold">Spotify</p>
+        </div>
+
+        <div className="border-secondary border-b py-5">
+          <p className="font-semibold">Apple Music</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MusicProvider;
