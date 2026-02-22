66890749ed64d7f7d11bf8d9863be66ff03bd51a
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2026-02-22 11:02:29 +0100

    made DietNutrientChart section responsive

--- fitness_platform/src/components/dietDetail/DietNutrientChart.jsx
+++ fitness_platform/src/components/dietDetail/DietNutrientChart.jsx
@@ -2,7 +2,7 @@ import DietNutrientChartCard from "./cards/DietNutrientChartCard";
 
 const DietNutrientChart = () => {
   return (
-    <div className="bg-accent mt-6 h[162.19px] w-[526.33px] rounded-[17px] p-3 grid grid-cols-4">
+    <div className="bg-accent mt-6 lg:h-[162.19px] lg:w-[526.33px] rounded-[17px] p-3 grid grid-cols-4 gap-2 lg:gap-0">
       <DietNutrientChartCard title="Calories" value="350" measurement="cal" />
       <DietNutrientChartCard title="Protein" value="25" measurement="gr" />
       <DietNutrientChartCard title="Carbs" value="10" measurement="gr" />
--- fitness_platform/src/components/dietDetail/cards/DietNutrientChartCard.jsx
+++ fitness_platform/src/components/dietDetail/cards/DietNutrientChartCard.jsx
@@ -1,9 +1,11 @@
 const DietNutrientChartCard = ({ title, value, measurement }) => {
   return (
     <div className="grid place-items-center py-5">
-      <p className="pb-4 text-center text-lg font-bold text-white">{title}</p>
-      <div className="flex h-[74.59px] w-[101.08px] flex-col items-center justify-center rounded-2xl bg-white">
-        <p className="text-lg font-bold">{value}</p>
+      <p className="pb-4 text-center font-bold text-white lg:text-lg">
+        {title}
+      </p>
+      <div className="flex h-full w-full flex-col items-center justify-center rounded-2xl bg-white p-2 lg:h-[74.59px] lg:w-[101.08px] lg:p-0">
+        <p className="font-bold lg:text-lg">{value}</p>
         <p className="text-sm font-medium text-[#9D9D9D]">{measurement}</p>
       </div>
     </div>
